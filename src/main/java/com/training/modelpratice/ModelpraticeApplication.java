f0cc5167454390b5fc5dc6d205d0c4ebfe406610
Arun <sankarun18@gmail.com>
2026-09-17 21:39:09 +0530

    samle commit

---
+++ src/main/java/com/training/modelpratice/ModelpraticeApplication.java
@@ -0,0 +1,14 @@
+package com.training.modelpratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelpraticeApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(ModelpraticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/modelpratice/ModelpraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.modelpratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelpraticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
