b5ebad8f068610bd7d7a3a576bdc9fd191057a04
Arun <sankarun18@gmail.com>
2026-09-17 21:41:26 +0530

    sample commit

--- src/main/java/com/training/modelpratice/ModelpraticeApplication.java
+++ src/main/java/com/training/modelpratice/ModelpraticeApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ModelpraticeApplication {
 
 	public static void main(String[] args) {
-
-        SpringApplication.run(ModelpraticeApplication.class, args);
+		SpringApplication.run(ModelpraticeApplication.class, args);
 	}
 
 }
